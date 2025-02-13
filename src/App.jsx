ece16a3df306608905fc914c25f3b4db4e90f605
Olena Ysynetska <olenayasynetska8@gmail.com>
2025-02-13 19:46:20 +0100

    ferst

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+import Rating from "./Rating";
+import List from "./List";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Рейтинг</h1>
+      <Rating />
+      <h1>Список приглашенных</h1>
+      <List />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/List/List.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./List.css";
+
+const List = () => {
+  const [people, setPeople] = useState([
+    { id: 1, name: "Иван", age: 20 },
+    { id: 2, name: "Мария", age: 22 },
+    { id: 3, name: "Алексей", age: 21 },
+    { id: 4, name: "Марина", age: 19 },
+    { id: 5, name: "Даша", age: 23 },
+    { id: 6, name: "Глеб", age: 24 },
+    { id: 7, name: "Дима", age: 18 },
+    { id: 8, name: "Гриша", age: 20 },
+    { id: 9, name: "Серафим", age: 21 }
+  ]);
+
+  const removePerson = (id) => {
+    setPeople(people.filter(person => person.id !== id));
+  };
+
+  return (
+    <div className="list-container">
+      <ul>
+        {people.map(({ id, name, age }) => (
+          <li key={id} className="list-item">
+            {name} ({age} лет)
+            <button onClick={() => removePerson(id)} className="remove-button">Удалить</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default List;
