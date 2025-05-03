861bb52e6c40e518583f08e0c3b0894f7ef7d16f
Olena Ysynetska <olenayasynetska8@gmail.com>
2025-05-03 14:59:08 +0200

    fertig

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,20 +4,9 @@ import { users } from "../../data/dataUsers";
 
 const List = () => {
    const [people, setPeople] = useState(users);
-   //([
-  //   { id: 1, name: "Иван", age: 20 },
-  //   { id: 2, name: "Мария", age: 22 },
-  //   { id: 3, name: "Алексей", age: 21 },
-  //   { id: 4, name: "Марина", age: 19 },
-  //   { id: 5, name: "Даша", age: 23 },
-  //   { id: 6, name: "Глеб", age: 24 },
-  //   { id: 7, name: "Дима", age: 18 },
-  //   { id: 8, name: "Гриша", age: 20 },
-  //   { id: 9, name: "Серафим", age: 21 }
-  // ]);
-
+   
   const removePerson = (id) => {
-    setPeople(people.filter(person => person.id !== id));
+    setPeople(prevPeople => prevPeople.filter(person => person.id !== id));
   };
 
   return (
